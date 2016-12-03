7ddcaa5c17ecbd96efab1d12d52fdf9405dec02a
khole <mail@interwebz.space>
2016-12-03 00:39:43 -0700

    making my main python app OOP instead of functional. Making a main section in the namespace to be imported into the flask template context

--- app/busbud.py
+++ app/busbud.py
@@ -12,53 +12,66 @@ from mylogins import *
 import time
 
 count = 0
+class Tripsdata:
+    # def __init__(self):
+    def pull(self):
+        '''Pull rtd-denver GTFS data and the assign bindings using gtfs_realtime_pb2'''
+        global count
+        while count == count:
+            tufeed = gtfs_realtime_pb2.FeedMessage()
+            print "Pulling the feed"
+            response = requests.get('http://www.rtd-denver.com/google_sync/TripUpdate.pb', auth=(username, passwords))
+            #Use response.content to load the binary into the feed object using gtfs_realtime_pb2 here.
+            tufeed.ParseFromString(response.content)
+            if tufeed == False:
+                print "nothing loaded"
+            else:
+                print "Feed is loaded now"
 
-def gettripupdate():
-    '''Pull rtd-denver GTFS data and the assign bindings using gtfs_realtime_pb2'''
-    global count
-    while count == count:
-        tufeed = gtfs_realtime_pb2.FeedMessage()
-        print "Pulling the feed"
-        response = requests.get('http://www.rtd-denver.com/google_sync/TripUpdate.pb', auth=(username, passwords))
-        #Use response.content to load the binary into the feed object using gtfs_realtime_pb2 here.
-        tufeed.ParseFromString(response.content)
-        if tufeed == False:
-            print "nothing loaded"
-        else:
-            print "Feed is loaded now"
+            return tufeed
+            time.sleep(30)
+            count += 1
+data = Tripsdata()
 
 
-        findarrival(tufeed)
-        time.sleep(30)
-        count += 1
+class Findarrival:
+    def list(self, tufeed):
+        self.times = []
+        '''Now we loop through and find our bus and the stop that we need'''
+        for entity in tufeed.entity:
+            #for mytrip in entity.trip_update.stop_time_update:
+            if entity.trip_update.trip.route_id == "0":
+                for mystop in entity.trip_update.stop_time_update:
+                    if mystop.stop_id == str("17780"):
+                        #print mystop.arrival.time
+                        self.times.append(mystop.arrival.time)
 
-def findarrival(tufeed):
-    times = []
-    '''Now we loop through and find our bus and the stop that we need'''
-    for entity in tufeed.entity:
-        #for mytrip in entity.trip_update.stop_time_update:
-        if entity.trip_update.trip.route_id == "0":
-            for mystop in entity.trip_update.stop_time_update:
-                if mystop.stop_id == str("17780"):
-                    #print mystop.arrival.time
-                    times.append(mystop.arrival.time)
-                    convertedtime = converttime(mystop.arrival.time)
-                    print "Arrival Time", convertedtime
-    findnextarrival(times)
-                #print "each route discription", mytrip
+                        # p?rint "Arrival Time", self.convertedtime
+        return self.times
+arrival = Findarrival()
+# arrival.list(tufeed)
 
-def findnextarrival(times):
-    ''' We simply find the lowest POSIX time and this will be our next bus'''
-    nexttime = min(times)
-    print "The next bus will be at it's stop at ", converttime(nexttime)
+class Nextarrival:
+    def mintime(self, time):
+        self.times = []
+        ''' We simply find the lowest POSIX time and this will be our next bus'''
+        nexttime = min(time)
+        return nexttime
+ournextarrival = Nextarrival()
 
-def converttime(ournexttime):
-    '''Here we convert the POSIX time from our GTFS feed to a date and time that we can read'''
-    convertedtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(ournexttime))
-    return convertedtime
-
+class Converttime:
+    def converter(self, ournexttime):
+        self.times = []
+        '''Here we convert the POSIX time from our GTFS feed to a date and time that we can read'''
+        convertedlist = []
+        for entry in self.times:
+            convertedlist = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(entry))
+        return convertedtime
+convertedtimes = Converttime()
 
 def main():
-    gettripupdate()
-
+    feed = data.pull()
+    listoftimes = arrival.times(feed)
+    mynextbus = ournextarrival.mintime(arrival.list.times)
+    myconvertedtimes = convertedtime.converter(listoftimes)
 main()
