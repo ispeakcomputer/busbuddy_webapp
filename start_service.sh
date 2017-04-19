a89cb5d196529d7c8eed0beb202da05da821b692
ryan <ryan@localhost.localdomain>
2017-04-19 08:43:25 -0600

    added note to tell user to open browser in the end of the script

--- start_service.sh
+++ start_service.sh
@@ -14,3 +14,5 @@ sudo celery multi start w1 -A __init__.celery worker --loglevel=info --concurren
 echo Lets now start Gunicorn
  sleep 7
 sudo gunicorn __init__:app
+
+echo PLEASE OPEN BROWSER TO 127.0.0.1:8000
