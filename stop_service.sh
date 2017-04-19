0bf975dd289565431ef7b33c20379af1ed0bd972
ryan <mail@interwebz.space>
2017-04-19 14:24:48 -0600

    Bash script to help us stop the service

---
+++ stop_service.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+echo Killing Celery Workers
+sudo pkill -f celery
+sleep 3
+echo Checking if any celerys are still running that need to be killed
+echo
+ps -aux|grep celery
+
+
+echo Killing RabbitMQ
+sudo pkill -f rabbitmq
+sleep 3
+echo Checking to see if RabbitMQ is still running. If so then kill the processes manualy
+echo
+
+ps -aux|grep rabbitmq
+
+
+echo Killing Gunicorn
+pkill -f gunicorn
+sleep 3
+ps -aux|grep gunicorn
