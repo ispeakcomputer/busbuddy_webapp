cea7a5bb03d7fa401a0d3bcbabb4e485e5ed0861
Ryan <ryan@ispeakcomputer.com>
2022-10-13 16:00:24 -0600

    clean up comments

--- backend.py
+++ backend.py
@@ -19,41 +19,26 @@ class Tripsdata:
 
         except requests.exceptions.Timeout:
             print "RTD Denver Data Timeout Trying Again In 60 Seconds"
-            # time.sleep(60)
 
         except requests.exceptions.TooManyRedirects:
             print "Too Many Redirects Trying Again In 60 Seconds"
-            # time.sleep(60)
 
 
         except requests.exceptions.RequestException as e:
             print str(e) + " Trying again in 60 Seconds"
-            # time.sleep(60)
 
         except requests.exceptions.HTTPError as err:
             print err
-            # time.sleep(60)
 
-        #Use response.content to load the binary into the feed object using gtfs_realtime_pb2 here.
         tufeed.ParseFromString(response.content)
         print "Parsing Data"
         return tufeed
 
-
-            # if tufeed == False:
-                # print "nothing loaded from Denver RTD retrying in 10 seconds"
-                # time.sleep(10)
-
-            # else:
-                # print "Feed is loaded now"
-                # self.count += 1
-                # return tufeed
 data = Tripsdata()
 
 class Feedstore:
     def get_packaged_data(self, tufeed):
         '''Parse through RTD data and start loading it into database'''
-        # model_thing.renew()
         model_thing.renew()
 
         for entity in tufeed.entity:
@@ -73,7 +58,3 @@ class Feedstore:
         model_thing.commit()
 
 feed = Feedstore()
-
-# if __name__ == '__main__':
-    # ourdata = data.pull()
-    # feed.get_packaged_data(ourdata)
